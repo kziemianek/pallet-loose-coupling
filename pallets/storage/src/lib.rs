617d89410699b72d8c980f0a6bc6abaf5e405fef
Kasper Ziemianek <kasper.ziemianek@gmail.com>
2023-03-01 23:53:29 +0100

    pallet loose coupling

--- pallets/storage/src/lib.rs
+++ pallets/storage/src/lib.rs
@@ -11,11 +11,15 @@ mod tests;
 #[cfg(feature = "runtime-benchmarks")]
 mod benchmarking;
 
+pub trait Storage {
+	fn store(something: u32);
+}
+
 #[frame_support::pallet]
 pub mod pallet {
+	use crate::Storage;
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
-	use pallet_storage::Storage;
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
@@ -24,7 +28,6 @@ pub mod pallet {
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
-		type Storage: Storage;
 	}
 
 	#[pallet::storage]
@@ -34,30 +37,36 @@ pub mod pallet {
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		/// Event documentation should end with an array that provides descriptive names for event
-		/// parameters. [something, who]
 		SomethingStored { something: u32, who: T::AccountId },
 	}
 
+	// Errors inform users that something went wrong.
 	#[pallet::error]
 	pub enum Error<T> {
 		NoneValue,
 		StorageOverflow,
 	}
 
+	impl<T: Config> Storage for Pallet<T> {
+		fn store(something: u32) {
+			<Something<T>>::put(something);
+		}
+	}
+
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
 		#[pallet::call_index(0)]
 		#[pallet::weight(10_000 + T::DbWeight::get().writes(1).ref_time())]
-		pub fn store_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
+		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
 			let who = ensure_signed(origin)?;
 
-			T::Storage::store(something);
+			<Something<T>>::put(something);
 
 			Self::deposit_event(Event::SomethingStored { something, who });
 			Ok(())
 		}
 
+		/// An example dispatchable that may throw a custom error.
 		#[pallet::call_index(1)]
 		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1).ref_time())]
 		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -41,14 +41,12 @@ pub use frame_support::{
 };
 pub use frame_system::Call as SystemCall;
 pub use pallet_balances::Call as BalancesCall;
+use pallet_storage::Storage;
 pub use pallet_timestamp::Call as TimestampCall;
 use pallet_transaction_payment::{ConstFeeMultiplier, CurrencyAdapter, Multiplier};
 #[cfg(any(feature = "std", test))]
 pub use sp_runtime::BuildStorage;
 pub use sp_runtime::{Perbill, Permill};
-
-/// Import the template pallet.
-pub use pallet_template;
 
 /// An index to a block.
 pub type BlockNumber = u32;
@@ -93,18 +91,11 @@ pub mod opaque {
 	}
 }
 
-// To learn more about runtime versioning, see:
-// https://docs.substrate.io/main-docs/build/upgrade#runtime-versioning
 #[sp_version::runtime_version]
 pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("node-template"),
 	impl_name: create_runtime_str!("node-template"),
 	authoring_version: 1,
-	// The version of the runtime specification. A full node will not attempt to use its native
-	//   runtime in substitute for the on-chain Wasm runtime unless all of `spec_name`,
-	//   `spec_version`, and `authoring_version` are the same between Wasm and native.
-	// This value is set to 100 to notify Polkadot-JS App (https://polkadot.js.org/apps) to use
-	//   the compatible custom types.
 	spec_version: 100,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
@@ -275,12 +266,15 @@ impl pallet_sudo::Config for Runtime {
 	type RuntimeCall = RuntimeCall;
 }
 
-/// Configure the pallet-template in pallets/template.
 impl pallet_template::Config for Runtime {
+	type RuntimeEvent = RuntimeEvent;
+	type Storage = pallet_storage::Pallet<Self>;
+}
+
+impl pallet_storage::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 }
 
-// Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
 	pub struct Runtime
 	where
@@ -296,8 +290,8 @@ construct_runtime!(
 		Balances: pallet_balances,
 		TransactionPayment: pallet_transaction_payment,
 		Sudo: pallet_sudo,
-		// Include the custom logic from the pallet-template in the runtime.
 		TemplateModule: pallet_template,
+		StorageModule: pallet_storage,
 	}
 );
 
@@ -345,6 +339,7 @@ mod benches {
 		[pallet_balances, Balances]
 		[pallet_timestamp, Timestamp]
 		[pallet_template, TemplateModule]
+		[pallet_storage, StorageModule]
 	);
 }
 
